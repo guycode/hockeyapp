24a9dd8b03319e903ac0c58e300d081205927632
Rich Lane <therealrichlane@gmail.com>
2015-04-10 22:24:14 -0600

    starting with templates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,19 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-end
 
-def show 
+  def show 
   @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new
+    @article.title = params[:article][:title]
+    @article.save
+    redirect_to article_path(@article)
+  end
 end
